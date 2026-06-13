d392e2518a14fa68d7a031c8dff019fdea0023b8
robikus <polakovic.robert.git@gmail.com>
2026-06-14 00:44:04 +0200

    updating production

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -28,11 +28,11 @@ DATABASES = {
 }
 
 # ── Security ──────────────────────────────────────────────────────────────────
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 0
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # ── CORS ──────────────────────────────────────────────────────────────────────
